9e79b9d6e5db535f6f937475794ea0c35d2fa6cb
Chloe Yip <yipcuni@gmail.com>
2023-10-22 10:53:54 -0400

    initial commit

---
+++ Button.py
@@ -0,0 +1,11 @@
+class Button() :
+    
+    pressed = False
+
+    def buttonPressed() :
+        pressed = True
+        return pressed
+    
+    def dayReset(medsList) :
+        for med in medsList :
+            med
